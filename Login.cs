dd295602e6601f99b4088b4f135f40e03b4c5906
RyanSimeon <ryansimeon@icloud.com>
2016-10-02 16:49:00 -0500

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace Project285
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="C: \Users\Ryan Simeon\documents\visual studio 2015\Projects\Project285\Project285\Data.mdf";Integrated Security=True;Connect Timeout=30");
+            //the only broken part of the code currently
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From Login where Username= ' " + textBox1.Text + "' and Password= '" + textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Main ss = new Main();
+                ss.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please check your username and password.");
+            }
+        }
+    }
+}
